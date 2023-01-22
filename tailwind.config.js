e96e16c088bfbc0f5a28b829ae6749469dc5e954
Kakon <pronobparsonal@gmail.com>
2023-01-22 13:59:36 +0600

    btn

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'primary': '#FF0000',
+          'secondary': '#FF8938',
+        },
+      },
     },
     plugins: [],
   }
